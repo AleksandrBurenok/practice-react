fbff570cdda837267a7378b7bfda9f65dbf884fe
Aleksandr <oleksandrburenok@gmail.com>
2019-10-01 14:26:38 +0300

    added validation props

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Lesson from './03_state/Lesson.jsx';
+import { Lesson, Counter, Button } from './05_props/Lesson.jsx';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Lesson />, document.getElementById('root'));
+ReactDOM.render(<Lesson child={<Button />}><Counter /></Lesson>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
