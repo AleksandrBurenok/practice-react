e62236b13a25e7523373933d5ecccc6fac17ad2f
Aleksandr <oleksandrburenok@gmail.com>
2019-10-01 13:46:28 +0300

    updated state and added functionality

--- src/03_state/Lesson.jsx
+++ src/03_state/Lesson.jsx
@@ -1,4 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const Counter = ({ counter, func, number, string }) => {
+  console.log(counter, func, number, string);
+  return <h1>{`Counter component. Counter value is: ${counter}`}</h1>
+}
+
+Counter.propTypes = {
+  counter: PropTypes.number.isRequired,
+  func: PropTypes.func,
+  number: PropTypes.number,
+  string: PropTypes.string,
+}
+
+Counter.defaultProps = {
+  func: () => {},
+  number: 0,
+  string: '',
+}
 
 class CounterButton extends Component {
   state = {
@@ -17,6 +36,9 @@ class CounterButton extends Component {
     return (
       <div>
         <div>{counter}</div>
+        <Counter
+          counter={counter}
+        />
         <button onClick={this.handleClick}>+1</button>
       </div>      
     );
