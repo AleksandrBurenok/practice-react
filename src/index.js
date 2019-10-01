85bc0544b1b8b83f421656b05dada335a4fd72ee
Aleksandr <oleksandrburenok@gmail.com>
2019-10-01 15:15:09 +0300

    added simple event

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Lesson, Counter, Button } from './05_props/Lesson.jsx';
+import App from './06_events/Lesson.jsx';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Lesson child={<Button />}><Counter /></Lesson>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
