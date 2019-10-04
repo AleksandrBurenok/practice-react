7a8d1e04f6d01e4bab665f64b1ec69c466488de9
Aleksandr <oleksandrburenok@gmail.com>
2019-10-04 12:28:32 +0300

    added form

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './09_fragments_&_css/Lesson.jsx';
+import App from './10_forms/Lesson.jsx';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
